daf0e42df6dcda44369b7c8c9fd542ba5ed3e141
Alizah Manag <2019079@icacademy.org>
2018-03-21 23:40:36 +0000

    added html structure and set up java functions

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//make sure put all JavaScript inside the function
+$(document).ready(function() {
+    
+    function userInput() {
+        $("input").val();
+    }
+    
+    function getDataFromApi(userInput) {
+        
+    }
+    
+    function displayUserData(data) {
+        
+    }
+    
+    
+    
+    
+    $.ajax({
+        url: "",
+        method: "GET",
+        success: function(response){
+          
+        },
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
