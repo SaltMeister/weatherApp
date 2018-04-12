86caeec7a550224bb52a7590b6b50c2e387e46a3
Alizah Manag <2019079@icacademy.org>
2018-04-12 00:21:58 +0000

    progress

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # weatherApp
 ScriptEd Weather App
+
 Project Link : https://docs.google.com/document/d/17BnlbmqwyEGndEhT5fbpV6jcebRv4LtDIqy7tdeFDYs/edit?usp=sharing
 
+Documentation for the API : https://www.wunderground.com/weather/api/d/docs?d=data/index
+Forecast Api = http://api.wunderground.com/api/13505fd272f8b46a/forecast/q/CA/94134.json
 Creators :
 Simon Huang
 Jason Giang
--- js/script.js
+++ js/script.js
@@ -1,34 +1,56 @@
+/*global $*/
 //make sure put all JavaScript inside the function
 $(document).ready(function() {
     
-    function userInput() {
-        $("input").val();
+    $("button").click(function() {
+        //alert("clicked");
+        getDataFromConditionApi(userInput());
+    });
+    $("input").keyup(function(event){
+    if(event.keyCode == 13){
+        $("button").click();
     }
-    
-    function getDataFromApi(userInput) {
-        
-    }
-    
-    function displayUserData(data) {
-        
-    }
-    
-    
-    
-    
-    $.ajax({
-        url: "",
-        method: "GET",
-        success: function(response){
-          
-        },
     });
     
+    //this Function takes in the User Input Data
+    function userInput() {
+        var userSearch = $("input").val();
+        return userSearch;
+        
+    }
+    
+    //Simon Is Working on this
+    //Function takes in the USer Input and puts it into the link
+    function getDataFromConditionApi(userInput) {
+        //makes are Variable that has the API linj
+        var apiUrl = "https://api.wunderground.com/api/13505fd272f8b46a/conditions/q/CA/" + userInput + ".json"; 
+       $.ajax({
+            url: apiUrl,
+            method: "GET",
+            success: function(response){
+              var temp = response.current_observation.temp_f;
+              var city = response.current_observation.display_location.city;
+              var logoWord = response.current_observation.icon;
+              var logo = response.current_observation.icon_url;
+              var zip = response.current_observation.display_location.zip
+              displayUserData(logoWord, logo, temp, city, zip);
+              //console.log(logoWord);
+            },
+    });
+    }
+    //Jason is working on this and It displays user data
+    function displayUserData(logoWord,logo, temp, city, zip) {
+        $("#results").html("<img src=" + logo + ">");//logo
+        $("#results").append(logoWord + " ");
+        $("#results").append(city + " ");//city name
+        $("#results").append(temp + "F");//temperature
+        $("#results").append(zip);
+    } 
     
     
     
     
-    
+      //getDataFromConditionApi(94112);
     
     
     
