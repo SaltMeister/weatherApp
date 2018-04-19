5ed4d7edecd8aa2af77e3d9f7367d2575b221af0
Alizah Manag <2019079@icacademy.org>
2018-04-19 00:22:55 +0000

    added css added more html and added forecast in javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 /*global $*/
 //make sure put all JavaScript inside the function
 $(document).ready(function() {
-    
-    $("button").click(function() {
+    $("h6").hide();
+    $("#buttonSearch").click(function() {
         //alert("clicked");
         getDataFromConditionApi(userInput());
     });
     $("input").keyup(function(event){
     if(event.keyCode == 13){
-        $("button").click();
+        $("#buttonSearch").click();
     }
+    });
+    $("#instruction").click(function() {
+        $("h6").toggle(1000);
     });
     
     //this Function takes in the User Input Data
@@ -23,9 +26,9 @@ $(document).ready(function() {
     //Function takes in the USer Input and puts it into the link
     function getDataFromConditionApi(userInput) {
         //makes are Variable that has the API linj
-        var apiUrl = "https://api.wunderground.com/api/13505fd272f8b46a/conditions/q/CA/" + userInput + ".json"; 
+        var apiConditionUrl = "https://api.wunderground.com/api/13505fd272f8b46a/conditions/q/CA/" + userInput + ".json"; 
        $.ajax({
-            url: apiUrl,
+            url: apiConditionUrl,
             method: "GET",
             success: function(response){
               var temp = response.current_observation.temp_f;
@@ -38,25 +41,40 @@ $(document).ready(function() {
             },
     });
     }
+    //this function gets the forecast from the userinput of the zip code
+    function getDataFromForecastApi(userInput) {
+        var apiForecastUrl = "https://api.wunderground.com/api/13505fd272f8b46a/forecast/q/CA/" + userInput + ".json";    
+       $.ajax({
+            url: apiForecastUrl,
+            method: "GET",
+            success: function(response){
+                for(var i = 0; i < response.forecast.simpleforecast.forecastday.length; i++){
+              var dayOfWeek = response.forecast.simpleforecast.forecastday[i].date[13];
+              var dayIcon = response.forecast.simpleforecast.forecastday[i].icon;
+              var dayIconImg = response.forecast.txt_forcast.forecastday[i].icon_url;
+              var dayHighTemp = response.forecast.txt_forcast.forecastday[i].high[0];
+              var dayLowTemp = response.forecast.txt_forcast.forecastday[i].low[0];
+                }
+                
+            },  
+       });
+    }
+    
+    
+    
     //Jason is working on this and It displays user data
     function displayUserData(logoWord,logo, temp, city, zip) {
-        $("#results").html("<img src=" + logo + ">");//logo
-        $("#results").append(logoWord + " ");
-        $("#results").append(city + " ");//city name
-        $("#results").append(temp + "F");//temperature
-        $("#results").append(zip);
-    } 
-    
-    
-    
-    
-      //getDataFromConditionApi(94112);
-    
-    
-    
-    
-    
-    
+        $("#dayWeather").html("<img src=" + logo + " height='100px' width='100px'>");//logo
+        $("#dayWeather").append(city + " ");//city name
+        $("#dayWeather").append(temp +"F ");//temperature
+        $("#dayWeather").append(zip);
+    }
+    function displayForecastData(dayWeek, dayIcon, dayIconImg, dayHighTemp, dayLowTemp){
+        $("#forecast").html("<img src=" + dayIconImg + "height='100px' width='100px'>");
+        $("#day").text(dayWeek);
+        $("")
+        
+    }
     
     
     
